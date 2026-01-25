cba8541af6865c3b61a9138307573e8d76474261
Richard Leek <richard@richardleek.com>
2026-01-25 23:06:08 +0100

    Merge password hasher

--- Tickflo.Core/Services/Authentication/IPasswordHasher.cs
+++
@@ -1,8 +0,0 @@
-namespace Tickflo.Core.Services.Authentication;
-
-public interface IPasswordHasher
-{
-    public string Hash(string input);
-    public bool Verify(string input, string hash);
-}
-
--- Tickflo.Core/Services/Authentication/PasswordHasher.cs
+++ Tickflo.Core/Services/Authentication/PasswordHasher.cs
@@ -2,6 +2,12 @@ namespace Tickflo.Core.Services.Authentication;
 
 using Isopoh.Cryptography.Argon2;
 
+public interface IPasswordHasher
+{
+    public string Hash(string input);
+    public bool Verify(string input, string hash);
+}
+
 public class Argon2idPasswordHasher : IPasswordHasher
 {
     public string Hash(string input) => Argon2.Hash(input);
