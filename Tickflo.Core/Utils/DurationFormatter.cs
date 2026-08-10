942ef174ab48d29424166071ecfa1a9b6ee05d61
Ryan <ryan@tickflo.co>
2026-08-10 15:53:59 -0400

    fix: add day formatting to DurationFormatter (#175)

--- Tickflo.Core/Utils/DurationFormatter.cs
+++ Tickflo.Core/Utils/DurationFormatter.cs
@@ -4,6 +4,12 @@ public static class DurationFormatter
 {
     public static string FormatExpiresIn(int maxAgeInSeconds)
     {
+        if (maxAgeInSeconds % 86400 == 0)
+        {
+            var days = maxAgeInSeconds / 86400;
+            return days == 1 ? "1 day" : $"{days} days";
+        }
+
         if (maxAgeInSeconds % 3600 == 0)
         {
             var hours = maxAgeInSeconds / 3600;
