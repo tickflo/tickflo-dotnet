ee9fa200b6e1860a5d8076a1d1b71398f519ae30
Ryan <ryan@tickflo.co>
2026-08-10 20:30:47 -0400

    fix: move using inside namespace declaration per IDE0065 (#187 CI)

--- Tickflo.Core/Migrations/20260810233047_AddWorkspaceIdToTicketInventory.cs
+++ Tickflo.Core/Migrations/20260810233047_AddWorkspaceIdToTicketInventory.cs
@@ -1,8 +1,8 @@
-using Microsoft.EntityFrameworkCore.Migrations;
-
 #nullable disable
 
 namespace Tickflo.Core.Migrations;
+
+using Microsoft.EntityFrameworkCore.Migrations;
 
 /// <inheritdoc />
 public partial class AddWorkspaceIdToTicketInventory : Migration
